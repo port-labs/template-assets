7b85cb91ad4c2eae3ae729a52b13dd0b6db98dc4
Matan Heled <matanheled@getport.io>
2023-03-02 08:23:55 +0200

    fixed sedush

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -100,7 +100,7 @@ temp_dir=$(mktemp -d)
 
 # Download config.yaml file into temporary folder
 curl ${CONFIG_YAML_URL} -o "${temp_dir}/config.yaml"
-cat "${temp_dir}/config.yaml"
+sed "s/{CLUSTER_NAME}/${CLUSTER_NAME}/g" "${temp_dir}/config.yaml"
 
 # Replace the place holder {CLUSTER_NAME} with passed cluster name in the config.yaml
 sed "s/{CLUSTER_NAME}/${CLUSTER_NAME}/g" "${temp_dir}/config.yaml" > "${temp_dir}/config.yaml"
