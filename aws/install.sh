82a06e91da589550656550beb48870094e49c0f2
talsabagport <tal@getport.io>
2023-03-07 17:31:44 +0200

    update aws install.sh

--- aws/install.sh
+++ aws/install.sh
@@ -20,7 +20,7 @@
 #   PORT_CLIENT_ID - Your Port organization Client ID (required)
 #   PORT_CLIENT_SECRET - Your Port organization Client Secret (required)
 #   EXPORTER_APP_NAME - The stack name of the application to create via AWS CloudFormation (default="port-aws-exporter")
-#   EXPORTER_BUCKET_NAME - The bucket name to create for the exporter configuration (default="port-aws-exporter-{128bit hash value}")
+#   EXPORTER_BUCKET_NAME - The bucket name to create for the exporter configuration (default="port-aws-exporter-{AWS_ACCOUNT_ID}")
 #   EXPORTER_LAMBDA_NAME - The function name to create for the exporter Lambda. (default="port-aws-exporter")
 #   EXPORTER_SECRET_NAME - The secret name to create for Port credentials (default="port-credentials")
 #   EXPORTER_IAM_POLICY_NAME - The IAM policy name to create for Lambda execution role (default="PortAWSExporterPolicy")
@@ -34,7 +34,6 @@ COMMON_FUNCTIONS_URL="${REPO_BASE_URL}/common.sh"
 
 # Exporter installation variables
 EXPORTER_APP_NAME=${EXPORTER_APP_NAME:-"port-aws-exporter"}
-EXPORTER_BUCKET_NAME=${EXPORTER_BUCKET_NAME:-"port-aws-exporter-$(echo -n "${PORT_CLIENT_SECRET}" | md5)"}
 EXPORTER_LAMBDA_NAME=${EXPORTER_LAMBDA_NAME:-"port-aws-exporter"}
 EXPORTER_SECRET_NAME=${EXPORTER_SECRET_NAME:-"port-credentials"}
 EXPORTER_IAM_POLICY_NAME=${EXPORTER_IAM_POLICY_NAME:-"PortAWSExporterPolicy"}
@@ -59,7 +58,19 @@ check_commands "aws" "jq"
 echo ""
 check_port_credentials "${PORT_CLIENT_ID}" "${PORT_CLIENT_SECRET}"
 
-echo "Download configuration files..."
+echo "Checking AWS login status..."
+echo ""
+if ! aws sts get-caller-identity
+then
+    echo ""
+    echo "aws sts get-caller-identity failed. Make sure you are logged in"
+    exit
+fi
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r ".Account")
+EXPORTER_BUCKET_NAME=${EXPORTER_BUCKET_NAME:-"port-aws-exporter-${AWS_ACCOUNT_ID}"}
+
+echo ""
+echo "Downloading configuration files..."
 echo ""
 save_endpoint_to_file "${REPO_AWS_CONTENT_URL}/config.json" "${temp_dir}/config.json"
 save_endpoint_to_file "${REPO_AWS_CONTENT_URL}/policy.json" "${temp_dir}/policy.json"
@@ -67,16 +78,16 @@ save_endpoint_to_file "${REPO_AWS_CONTENT_URL}/parameters.json" "${temp_dir}/par
 save_endpoint_to_file "${REPO_AWS_CONTENT_URL}/event_rules.yaml" "${temp_dir}/event_rules.yaml"
 
 echo ""
-echo "Creating IAM Policy"
+echo "Creating IAM policy..."
 if ! aws iam create-policy --policy-name "${EXPORTER_IAM_POLICY_NAME}" --policy-document "file://${temp_dir}/policy.json"
 then
     echo ""
     echo "Warning: policy creation failed, might already exists. Skipping to the next step"
 fi
-EXPORTER_IAM_POLICY_ARN="arn:aws:iam::$(aws sts get-caller-identity | jq -r ".Account"):policy\/${EXPORTER_IAM_POLICY_NAME}"
+EXPORTER_IAM_POLICY_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:policy\/${EXPORTER_IAM_POLICY_NAME}"
 
 echo ""
-echo "Preparing parameters json file"
+echo "Preparing parameters json file..."
 echo ""
 
 sed -i.backup \
@@ -90,34 +101,37 @@ cat "${temp_dir}/parameters.json"
 echo ""
 
 echo ""
-echo "Deploying Port's AWS exporter application"
+echo "Deploying Port's AWS exporter application..."
 
 CHANGE_SET_ID=$(aws serverlessrepo create-cloud-formation-change-set \
 --application-id arn:aws:serverlessrepo:eu-west-1:185657066287:applications/port-aws-exporter \
 --stack-name "${EXPORTER_APP_NAME}" --capabilities CAPABILITY_IAM CAPABILITY_RESOURCE_POLICY \
 --parameter-overrides "file://${temp_dir}/parameters.json" | jq -r ".ChangeSetId")
 
-aws cloudformation wait change-set-create-complete --change-set-name "${CHANGE_SET_ID}"
-
-aws cloudformation execute-change-set --change-set-name "${CHANGE_SET_ID}"
+if ! aws cloudformation wait change-set-create-complete --change-set-name "${CHANGE_SET_ID}" &> /dev/null
+then
+    echo ""
+    aws cloudformation describe-change-set --change-set-name "${CHANGE_SET_ID}" | jq -c 'with_entries(select([.key] | inside(["Status", "StatusReason"])))'
+else
+    aws cloudformation execute-change-set --change-set-name "${CHANGE_SET_ID}"
+    echo ""
+    cloudformation_tail "serverlessrepo-${EXPORTER_APP_NAME}"
+fi
 
 echo ""
-cloudformation_tail "serverlessrepo-${EXPORTER_APP_NAME}"
-
-echo ""
-echo "Uploading config.json to S3 bucket"
+echo "Uploading config.json to S3 bucket..."
 echo ""
 
 aws s3api put-object --bucket "${EXPORTER_BUCKET_NAME}" --key "config.json" --body "${temp_dir}/config.json" || exit 1
 
 echo ""
-echo "Updating Port credentials secret"
+echo "Updating Port credentials secret..."
 echo ""
 
 aws secretsmanager put-secret-value --secret-id "${EXPORTER_SECRET_NAME}" --secret-string "{\"id\":\"${PORT_CLIENT_ID}\",\"clientSecret\":\"${PORT_CLIENT_SECRET}\"}" || exit 1
 
 echo ""
-echo "Creating CloudFormation of event rules for real-time updates"
+echo "Creating CloudFormation of event rules for real-time updates..."
 
 aws cloudformation deploy --template-file "${temp_dir}/event_rules.yaml" --stack-name "${EXPORTER_CF_EVENT_RULES_STACK_NAME}"
 
@@ -125,21 +139,18 @@ echo ""
 echo "Finished installation!"
 
 echo ""
-echo "Run first test"
+echo "Running exporter's lambda manually..."
 echo ""
 
-aws s3api wait object-exists --bucket "${EXPORTER_BUCKET_NAME}" --key "config.json"
-
-# Checks if AWS SAM CLI is installed
 if ! command -v sam &> /dev/null
 then
-    echo "sam command is not exists, can't tail live logs of exporter's lambda"
+    echo "sam command for live logs does not exists, will print the execution log's tail when done"
     echo ""
     aws lambda invoke --function-name "${EXPORTER_LAMBDA_NAME}" --log-type "Tail" --payload "{}" "${temp_dir}/outfile" | jq -r ".LogResult" | base64 --decode
 else
     aws lambda invoke --function-name "${EXPORTER_LAMBDA_NAME}" --invocation-type "Event" --payload "{}" "${temp_dir}/outfile"
     echo ""
-    echo "Tail exporter's lambda logs, press CTRL+C to break"
+    echo "Tail exporter's lambda logs, press CTRL+C to break..."
     echo ""
     sam logs --stack-name "serverlessrepo-${EXPORTER_APP_NAME}" --tail
 fi
