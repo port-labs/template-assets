749530b2d1642bf0b61632859417b5cc71722314
talsabagport <tal@getport.io>
2023-03-07 14:33:06 +0200

    update aws install.sh

--- aws/install.sh
+++ aws/install.sh
@@ -50,7 +50,7 @@ temp_dir=$(mktemp -d)
 
 echo "Importing common functions..."
 curl -s ${COMMON_FUNCTIONS_URL} -o "${temp_dir}/common.sh"
-source "../common.sh"
+source "${temp_dir}/common.sh"
 
 echo ""
 echo "Checking for prerequisites..."
