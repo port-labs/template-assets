bc97f2bffc5e33385ceb183a6ff06c734ad79950
Rob de Heer <robertsdeheer@gmail.com>
2023-03-25 17:23:48 -0700

    main: Added stackSegment

--- aws/install.sh
+++ aws/install.sh
@@ -29,7 +29,7 @@
 
 # Global variables
 REPO_BRANCH=${REPO_BRANCH:-"main"}
-REPO_BASE_URL="https://raw.githubusercontent.com/port-labs/template-assets/${REPO_BRANCH}"
+REPO_BASE_URL="https://raw.githubusercontent.com/AutoFi/template-assets/${REPO_BRANCH}"
 REPO_AWS_CONTENT_URL="${REPO_BASE_URL}/aws"
 COMMON_FUNCTIONS_URL="${REPO_BASE_URL}/common.sh"
 
