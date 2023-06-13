3dc3f633d44427c94c525592ca5f29b03b0935f7
yair <yairsimantov20@gmail.com>
2023-06-13 12:32:36 +0300

    comment

--- gitlab/gitlab_exporter.py
+++ gitlab/gitlab_exporter.py
@@ -156,6 +156,7 @@ def process_data_from_all_groups_from_gitlab():
 
         print(f"Matching groups: {configured_groups}")
 
+        # Creating webhooks and getting projects for each group
         for group in configured_groups:
             # Create webhook for root groups
             if SKIP_WEBHOOK_CREATION != 'true' and group['id'] in root_groups_ids:
