4332ab22057646d6a1089d87b7d110e9e16bbcaf
Rob de Heer <robertsdeheer@gmail.com>
2023-07-18 22:08:33 -0700

    template-assets: Updated elastic dns after removing alias

---
+++ aws/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  #   region = var.region
+
+  default_tags {
+    tags = {
+      CostCenter = "Overdrive"
+    }
+  }
+}
