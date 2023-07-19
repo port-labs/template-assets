61b5fbbe841a66e002d5bfe39e0e510334c92dbb
Rob de Heer <robertsdeheer@gmail.com>
2023-07-18 22:45:32 -0700

    template-assets: Updated elastic dns after removing alias

--- aws/aws_blueprints_template/main.tf
+++ aws/aws_blueprints_template/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     port-labs = {
-          source = "port-labs/port-labs"
-          version = "0.10.4"
+      source  = "port-labs/port-labs"
+      version = "0.10.4"
     }
   }
 }
@@ -16,17 +16,18 @@ resource "port-labs_blueprint" "region" {
   identifier = "port_aws_region"
 
   properties {
-    title = "Link"
+    title      = "Link"
     identifier = "link"
-    type = "string"
-    format = "url"
+    type       = "string"
+    format     = "url"
   }
 }
 
 resource "port-labs_entity" "current_region" {
   identifier = data.aws_region.current.name
-  title     = data.aws_region.current.name
-  blueprint = port-labs_blueprint.region.identifier
+  title      = data.aws_region.current.name
+  blueprint  = port-labs_blueprint.region.identifier
+
   properties {
     name  = "link"
     value = "https://${data.aws_region.current.name}.console.aws.amazon.com/"
@@ -35,55 +36,55 @@ resource "port-labs_entity" "current_region" {
 
 
 module "port_dynamodb_table" {
-  source = "../dynamodb_table"
-  count = contains(var.resources, "dynamodb_table") ? 1 : 0
+  source     = "../dynamodb_table"
+  count      = contains(var.resources, "dynamodb_table") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_ecs_service" {
-  source = "../ecs_service"
-  count = contains(var.resources, "ecs_service") ? 1 : 0
+  source     = "../ecs_service"
+  count      = contains(var.resources, "ecs_service") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_lambda_function" {
-  source = "../lambda"
-  count = contains(var.resources, "lambda") ? 1 : 0
+  source     = "../lambda"
+  count      = contains(var.resources, "lambda") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_rds_db_instance" {
-  source = "../rds_db_instance"
-  count = contains(var.resources, "rds_db_instance") ? 1 : 0
+  source     = "../rds_db_instance"
+  count      = contains(var.resources, "rds_db_instance") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_s3_bucket" {
-  source = "../s3_bucket"
-  count = contains(var.resources, "s3_bucket") ? 1 : 0
+  source     = "../s3_bucket"
+  count      = contains(var.resources, "s3_bucket") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_sqs" {
-  source = "../sqs"
-  count = contains(var.resources, "sqs") ? 1 : 0
+  source     = "../sqs"
+  count      = contains(var.resources, "sqs") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_sns" {
-  source = "../sns"
-  count = contains(var.resources, "sns") ? 1 : 0
+  source     = "../sns"
+  count      = contains(var.resources, "sns") ? 1 : 0
   depends_on = [port-labs_blueprint.region, module.port_sqs]
 }
 
 module "port_ec2_instance" {
-  source = "../ec2_instance"
-  count = contains(var.resources, "ec2_instance") ? 1 : 0
+  source     = "../ec2_instance"
+  count      = contains(var.resources, "ec2_instance") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
 
 module "port_load_balancer" {
-  source = "../load_balancer"
-  count = contains(var.resources, "load_balancer") ? 1 : 0
+  source     = "../load_balancer"
+  count      = contains(var.resources, "load_balancer") ? 1 : 0
   depends_on = [port-labs_blueprint.region]
 }
--- aws/providers.tf
+++ aws/providers.tf
@@ -1,18 +1,12 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.8.0"
-    }
-  }
-}
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.8.0"
+#     }
+#   }
+# }
 
 provider "aws" {
   #   region = var.region
-
-  default_tags {
-    tags = {
-      CostCenter = "Overdrive"
-    }
-  }
 }
