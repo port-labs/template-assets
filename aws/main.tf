9309b86fc0ce1fe4e3896cedd79567602838c23f
Laurent Sibilla <laurent@sibilla.be>
2024-05-31 16:53:17 +0200

    fix: remove tofu provider mapping warning

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.1.0"
+    }
     local = {
       source = "hashicorp/local"
       version = "2.4.0"
